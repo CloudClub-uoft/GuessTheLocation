0051c29c3657a91273b27959aca985d75c31b9cf
John Hao <johnhaoallwood@gmail.com>
2024-01-05 10:48:48 -0500

    changed test cases: user authentication now working

--- server/tests/pythontest.py
+++ server/tests/pythontest.py
@@ -13,13 +13,37 @@ def testRegister():
 
     post_response = requests.post(url_post, json=data)
 
-    post_response_json = post_response.text
-    print(post_response_json)
+    print("register:", post_response.json())
 
-def testDelete(idd):
+def testDelete(idd, cookies=None):
+    session = requests.session()
+    if cookies:
+        session.cookies.update(cookies)
+
     url = f"http://localhost:3001/users/{idd}"
-    response = requests.delete(url)
-    print(response.text)
+    response = session.delete(url)
+    print("delete:", response.json())
+
+
+def testLogin():
+    session = requests.session()
+
+    data = {
+        "username": "one",
+        "password": "passw0rd"
+    }
+
+    url_post = "http://localhost:3001/auth/login"
+
+    post_response = session.post(url_post, json=data)
+
+    post_response_json = post_response.json()
+    print("login cookies:", session.cookies)
+    print("login response:", post_response_json)
+    return post_response_json, session.cookies
 
 testRegister()
-#testDelete(input())
+response, cookies = testLogin()
+
+#testDelete(response["userID"], cookies)  # successful
+#testDelete(response["userID"])  # not successful

150137ef2c8624b6330ffa93d789379827285d88
Brady Zhang <bradygreat4@gmail.com>
2024-02-23 23:41:48 -0500

    back-end route to get image from s3

---
+++ server/routes/GET-image.js
@@ -0,0 +1,27 @@
+module.exports = (app, s3Client) => {
+  app.get("/getimage", (req, res) => {
+    // var image;
+    var filename = req.query.key;
+
+    // s3 get request params
+    var params = {
+      Bucket: process.env.BUCKET_NAME,
+      Key: filename,
+    };
+
+    s3Client.getObject(params, (err, data) => {
+      if (err) {
+        console.log(err);
+        res.send({});
+      } else {
+        console.log("Got image: " + req.query.key);
+        let content = data.Body; // raw data of the image
+        let fileExt = filename.substring(filename.lastIndexOf(".") + 1); // get file suffix
+        res.send({
+          image: content, // send to React front-end
+          fileExtenstion: fileExt,
+        });
+      }
+    });
+  });
+};
